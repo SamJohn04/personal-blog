3b3ecf46af2069cb58cf813bc4ec8acf6acbf3e6
SamJohn04 <samuel.john.codes@gmail.com>
2025-11-17 07:47:27 +0530

    feat: blog page to contain edit and delete blog options for authLevel 3

--- src/frontend/src/pages/Blog.tsx
+++ src/frontend/src/pages/Blog.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import DefaultHeader from "../components/DefaultHeader";
 import { useEffect, useState } from "react";
 
@@ -11,8 +11,11 @@ type Blog = {
 }
 
 export default function Blog() {
+  const navigate = useNavigate();
+
   const [blog, setBlog] = useState<Blog | null>(null);
   const { id } = useParams();
+
   useEffect(() => {
     async function getBlog() {
       const res = await fetch(`/api/blog/${id}`);
@@ -30,12 +33,37 @@ export default function Blog() {
     }
     getBlog();
   }, []);
+
+  async function deleteBlog() {
+    if (!confirm("Are you sure you want to delete the blog?")) {
+      return;
+    }
+    const res = await fetch(`/api/blog/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem("authToken")}`,
+      }
+    });
+    if (res.ok) {
+      navigate("/");
+    } else {
+      alert("Something went wrong");
+    }
+  }
+
   return (
     <>
       <DefaultHeader/>
       <main>
         <h1>{blog?.title}</h1>
         <p className="text-gray">{blog?.createdAt?.toDateString()}</p>
+        {
+          localStorage.getItem("authLevel") === "3" &&
+            <div className="pad-b-10 grid cols-2 gap-4">
+          <Link to={`/blog/${id}/edit`} className="grid"><button>Edit Blog</button></Link>
+          <button onClick={deleteBlog}>Delete Blog</button>
+          </div>
+        }
         <div dangerouslySetInnerHTML={{ __html: blog?.content ?? "" }} />
       </main>
     </>

704732b6f1681c02bf16f05cccda287bd51a6410
SamJohn04 <samuel.john.codes@gmail.com>
2025-11-17 07:44:03 +0530

    feat: logout button

--- src/frontend/src/components/DefaultHeader.tsx
+++ src/frontend/src/components/DefaultHeader.tsx
@@ -1,11 +1,23 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 export default function DefaultHeader() {
+  const navigate = useNavigate();
   const email = getLoggedInEmail();
+  
+  function logout() {
+    localStorage.removeItem("authLevel");
+    localStorage.removeItem("authToken");
+    localStorage.removeItem("email");
+
+    navigate("/");
+  }
+
   return (
     <header>
       <span id="header-text"><Link to="/">Samuel's Mind</Link></span>
-      <span id="email">{email ? email : <Link to="/signup">Sign Up</Link>}</span>
+      <span id="email">{email ?
+        <span>{email} <button className="min-y-pad" onClick={logout}>Log Out</button></span>
+          : <Link to="/signup">Sign Up</Link>}</span>
     </header>
   )
 }
